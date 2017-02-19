49602c70dbcf74b482494417874250173b12148b
Hryshchuk Bohdan <bohdanuk16@gmail.com>
2017-02-19 15:24:49 +0200

    1.4_Smarty

--- config/config.php
+++ config/config.php
@@ -3,3 +3,23 @@
 // константы обращения к контролерам
 define('PathPrefix', '../controllers/');
 define('PathPostfix', 'Controller.php');
+
+//> Используемый шаблон
+$template = 'default';
+
+// путь к файлам шаблонов ТП1
+define('TemplatePrefix', '../views/'.$template.'/');
+define('TemplatePostfix', '.tp1');
+
+// путь к файлам шаблонов в вебе
+define('TemplateWebPath', '../templates/'.$template.'/');
+
+require_once('../library/Twig/libs/Smarty.class.php');
+$smarty = new Smarty();
+
+$smarty->setTemplaterDir(TemplatePrefix);
+$smarty->setCompileDir('../tmp/smarty/templates_c');
+$smarty->setCacheDir('../tmp/smarty/cache');
+$smarty->setConfigDir('../tmp/smarty/configs');
+
+$smarty->assing('templateWebPath', TemplateWebPath);
