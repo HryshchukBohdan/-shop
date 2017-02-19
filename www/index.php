374474445846a67d825260c9c81c41e229a9c6bb
Hryshchuk Bohdan <bohdanuk16@gmail.com>
2017-02-19 13:46:45 +0200

    1.1_N_controller

--- www/index.php
+++ www/index.php
@@ -1,2 +1,9 @@
 <?php
-phpinfo();
+
+$contrullerName = isset($_GET['controler']) ? ucfirst($_GET['controler']) : 'Index';
+echo "2017" . $contrullerName . "<br>";
+
+$actionName = isset($_GET['action']) ? ucfirst($_GET['action']) : 'Index';
+echo "2017" . $actionName . "<br>";
+
+include_once '../controllers/' . $contrullerName . 'Controller.php';
